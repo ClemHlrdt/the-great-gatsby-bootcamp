bb7e8f8954e38a4895039f87d35aa514a0fdd774
Clément Hélardot <clement@MacBook-Pro-de-Clement.local>
2019-11-19 17:42:06 +0100

    Contentful integration & custom 404

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,22 +1,22 @@
 // file to generate slugs and blog post pages
 const path = require('path')
 
-module.exports.onCreateNode = ({ node, actions }) => {
-  // when a node is created...
-  const { createNodeField } = actions
-  /* 
-    for file with internal type === MarkdownRemark, 
-    We create a slug 
-  */
-  if (node.internal.type === "MarkdownRemark") {
-    const slug = path.basename(node.fileAbsolutePath, '.md');
-    createNodeField({
-        node,
-        name: 'slug',
-        value: slug
-    })
-  }
-}
+// module.exports.onCreateNode = ({ node, actions }) => {
+//   // when a node is created...
+//   const { createNodeField } = actions
+//   /* 
+//     for file with internal type === MarkdownRemark, 
+//     We create a slug 
+//   */
+//   if (node.internal.type === "MarkdownRemark") {
+//     const slug = path.basename(node.fileAbsolutePath, '.md');
+//     createNodeField({
+//         node,
+//         name: 'slug',
+//         value: slug
+//     })
+//   }
+// }
 
 module.exports.createPages = async ({ graphql, actions }) => {
     const { createPage } = actions;
@@ -25,25 +25,23 @@ module.exports.createPages = async ({ graphql, actions }) => {
     // 2. Get markdown data
     const response = await graphql(`
       query {
-        allMarkdownRemark {
+        allContentfulBlogPost {
           edges {
             node {
-              fields {
-                slug
-              }
+              slug
             }
           }
         }
       }
-    `);
+    `)
     // 3. Create new pages
-    response.data.allMarkdownRemark.edges.forEach((edge) => {
-        createPage({
-            component: blogTemplate,                    // pass the template
-            path: `/blog/${edge.node.fields.slug}`,     // create a pathname
-            context: {
-                slug: edge.node.fields.slug             // pass some context info
-            }
-        })
+    response.data.allContentfulBlogPost.edges.forEach(edge => {
+      createPage({
+        component: blogTemplate, // pass the template
+        path: `/blog/${edge.node.slug}`, // create a pathname
+        context: {
+          slug: edge.node.slug, // pass some context info
+        },
+      })
     })
 }
--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,32 +1,63 @@
 import React from 'react';
 import { graphql } from 'gatsby'; 
 import Layout from '../components/layout';
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+// export const query = graphql`
+//   query($slug: String!) {
+//     markdownRemark(fields: { slug: { eq: $slug } }) {
+//       frontmatter {
+//         title
+//         date
+//       }
+//       html
+//     }
+//   }
+// `
+
+// const Blog = (props) => {
+
+//     return (
+//       <Layout>
+//         <h1>{props.data.markdownRemark.frontmatter.title}</h1>
+//         <p>{props.data.markdownRemark.frontmatter.date}</p>
+//         <div
+//           dangerouslySetInnerHTML={{
+//             __html: props.data.markdownRemark.html
+//           }}
+//         ></div>
+//       </Layout>
+//     )
+// }
 
 export const query = graphql`
-  query($slug: String!) {
-    markdownRemark(fields: { slug: { eq: $slug } }) {
-      frontmatter {
-        title
-        date
+         query($slug: String!) {
+           contentfulBlogPost(slug: { eq: $slug }) {
+             title
+             publishedDate(formatString: "DD/MM/YYYY")
+             body {
+               json
+             }
+           }
+         }
+       `
+
+const Blog = props => {
+  const options = {
+    renderNode: {
+      "embedded-asset-block": (node) => {
+        const alt = node.data.target.fields.title['en-US'];
+        const url = node.data.target.fields.file['en-US'].url;
+        return <img alt={alt} src={url}/>
       }
-      html
     }
   }
-`
-
-const Blog = (props) => {
-
-    return (
-      <Layout>
-        <h1>{props.data.markdownRemark.frontmatter.title}</h1>
-        <p>{props.data.markdownRemark.frontmatter.date}</p>
-        <div
-          dangerouslySetInnerHTML={{
-            __html: props.data.markdownRemark.html
-          }}
-        ></div>
-      </Layout>
-    )
+  return (
+    <Layout>
+      <h1>{props.data.contentfulBlogPost.title}</h1>
+      <p>{props.data.contentfulBlogPost.publishedDate}</p>
+      {documentToReactComponents(props.data.contentfulBlogPost.body.json, options)}
+    </Layout>
+  )
 }
 
 export default Blog;
