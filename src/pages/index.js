3f60fd4c73eec3ee9df3cd6a263775972e56ae9b
Clément Hélardot <clement@MacBook-Pro-de-Clement.local>
2019-11-19 18:38:22 +0100

    remove paragraph in index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,6 @@ const IndexPage = () => {
             <Head title="Home" />
             <h1>Hello.</h1>
             <h2>I'm Andrew, a full-stack developer living in beautiful Philadelphia.</h2>
-            <p>Need a developer? <Link to="/contact">Contact me.</Link></p>
         </Layout>
     )
 }
